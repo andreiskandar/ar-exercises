87e6b0a1f0a1041c5cea3efa9070db3e50e2bfef
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-10-12 16:55:36 -0700

    completed ex1

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,17 @@
 class Store < ActiveRecord::Base
+  
+  attr_accessor :name, :annual_revenue, :mens_apparel, :womens_apparel
 
+  # def initialize(name, annual_revenue, mens_apparel, womens_apparel)
+  #   @name = name
+  #   @annual_revenue = annual_revenue
+  #   @mens_apparel = mens_apparel
+  #   @womens_apparel = womens_apparel
+  # end
+
+  # def info
+  #   "#{name} #{annual_revenue} womens_apparel:#{womens_apparel} mens_apparel:#{mens_apparel}"
+  # end
+
+    
 end
